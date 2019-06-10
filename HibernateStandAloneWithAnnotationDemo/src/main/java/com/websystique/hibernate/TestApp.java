b428decd17fbe2c7f0d7546b2e3bd8ac7af2b8e6
chiragj <chirag.j@ezdi.us>
2019-06-10 12:43:24 +0530

    initial commit

---
+++ HibernateStandAloneWithAnnotationDemo/src/main/java/com/websystique/hibernate/TestApp.java
@@ -0,0 +1,26 @@
+package com.websystique.hibernate;
+
+import com.websystique.hibernate.dao.CompanyDao;
+import com.websystique.hibernate.dao.EmpDao;
+
+public class TestApp {
+
+	public static void main(String[] args) {
+		
+		EmpDao dao = new EmpDao();
+		//dao.getAllEmpDetails();
+		//dao.saveEmp();
+		//dao.updateEmp();
+		//dao.deleteEmp();
+		dao.getEmployee();
+	}
+	
+	public static void main2(String[] args) {
+		CompanyDao companyDao = new CompanyDao();
+		companyDao.updateCompanyEmployee();
+		//companyDao.insertComapny();
+	}
+	
+	
+	
+}
